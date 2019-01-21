f8af724003a41ebab6be331a9b50d855acaaa4bf
Boris Baldassari <boris.baldassari@gmail.com>
2019-01-21 18:09:26 +0100

    Fix script header for get_api_forums.

--- forums.eclipse.org/get_api_forums.sh
+++ forums.eclipse.org/get_api_forums.sh
@@ -1,4 +1,15 @@
 
+######################################################################
+# Copyright (c) 2017 Castalia Solutions
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+######################################################################
+
+
 echo "Get info for all categories:";
 curl -s https://api.eclipse.org/forums/category -o mls_cats.json
 # Returns
